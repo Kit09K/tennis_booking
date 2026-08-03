b3751227acbf014e10f65cd13724528b364d33bd
Patchara Taksinapinunt <Taskinapinuntpatchara@gmail.com>
2026-08-03 13:25:43 +0700

    fix seed data

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :keycloak_openid,
-           ENV.fetch('KEYCLOAK_CLIENT_ID', ''),
-           ENV.fetch('KEYCLOAK_CLIENT_SECRET', ''),
+           ENV.fetch("KEYCLOAK_CLIENT_ID", ""),
+           ENV.fetch("KEYCLOAK_CLIENT_SECRET", ""),
            client_options: {
-             site: 'https://sso-dev.odd.works',
-             realm: 'odt',
-             base_url: ''
+             site: "https://sso-dev.odd.works",
+             realm: "odt",
+             base_url: ""
            },
-           name: 'keycloak'
+           name: "keycloak"
 end
 
 # Rails.application.config.middleware.use OmniAuth::Builder do
